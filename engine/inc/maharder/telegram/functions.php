b55275318e7be1833e8d486a21d678d8a862db42
MaHarder <info@maxim-harder.de>
2018-05-23 08:19:51 +0200

    НОвый файл с функциями для телеграма. Прокси

---
+++ engine/inc/maharder/telegram/functions.php
@@ -0,0 +1,73 @@
+<?php
+//	===============================
+//	Скрипт функций модуля
+//	===============================
+//	Автор: Maxim Harder
+//	Сайт: https://maxim-harder.de
+//	Телеграм: http://t.me/MaHarder
+//	===============================
+//	Ничего не менять
+//	===============================
+
+if(!function_exists('sendMessage')) {
+    function sendMessage ($url) {
+        global $telebot;
+        if($telebot['method'] == 1) {
+            if($telebot['proxy']) {
+                if($telebot['proxyauth']) {
+                    $auth = base64_encode($telebot['proxyuser'] . ':' . $telebot['proxypass']);
+                    $opts = array(
+                        'http' => array (
+                            'method'=>'GET',
+                            'proxy'=>'tcp://' . $telebot['proxyip'] . ':' . $telebot['proxyport'],
+                            'request_fulluri' => true,
+                            'header'=> array("Proxy-Authorization: Basic $auth", "Authorization: Basic $auth")
+
+                        ),
+                        'https' => array (
+                            'method'=>'GET',
+                            'proxy'=>'tcp://' . $telebot['proxyip'] . ':' . $telebot['proxyport'],
+                            'request_fulluri' => true,
+                            'header'=> array("Proxy-Authorization: Basic $auth", "Authorization: Basic $auth")
+                        )
+                    );
+                } else {
+                    $opts = array(
+                        'http' => array (
+                            'method'=>'GET',
+                            'proxy'=>'tcp://' . $telebot['proxyip'] . ':' . $telebot['proxyport'],
+                            'request_fulluri' => true
+
+                        ),
+                        'https' => array (
+                            'method'=>'GET',
+                            'proxy'=>'tcp://' . $telebot['proxyip'] . ':' . $telebot['proxyport'],,
+                            'request_fulluri' => true
+                        )
+                    );
+                }
+                $context = stream_context_create($opts);
+                $content = file_get_contents($url, false, $context);
+            } else {
+                $content = file_get_contents($url);
+            }
+        } elseif ($telebot['method'] == 2) {
+            if($telebot['proxy']) $proxy = $telebot['proxyip'] . ':' . $telebot['proxyport'];
+            if($telebot['proxyauth']) $proxyauth = $telebot['proxyuser'] . ':' . $telebot['proxypass'];
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            if($telebot['proxy']) curl_setopt($ch, CURLOPT_PROXY, $proxy);
+            if($telebot['proxyauth']) curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+            curl_setopt($ch, CURLOPT_HEADER, 1);
+            $content = curl_exec($ch);
+            curl_close($ch);
+        }
+
+        return $content;
+    }
+}
+
+?>
--- engine/inc/maharder/telegram/sendMessage.php
+++ engine/inc/maharder/telegram/sendMessage.php
@@ -11,6 +11,7 @@
 //	===============================
 
 if(!$telebot['token'] && !$telebot['chat']) messageOut("Нехватает настроек", "Убедитесь, что вы сохранили токен бота и ID чата.", array($adminlink => "Настройки"));
+require_once (ENGINE_DIR . '/inc/maharder/telegram/functions.php');
 
 switch ($_GET['action']) {
 	case 'send':
@@ -26,7 +27,7 @@ switch ($_GET['action']) {
 		$temes = preg_replace("/\[url=(.*)\](.*)\[\/url\]/", "<a href=\"$1\">$2</a>", $temes);
 		$temes = preg_replace("/\[url\](.*)\[\/url\]/", "<a href=\"$1\">$1</a>", $temes);
 		$turl = "https://api.telegram.org/bot". $telebot['token'] ."/sendMessage?chat_id=". $telebot['chat'] ."&text=" . urlencode ( $temes ) . "&parse_mode=HTML";
-		file_get_contents($turl);
+        sendMessage($turl);
 		messageOut("Сообщение отправлено", "Дообщение было отправлено в чат.", array($adminlink => "Настройки", $adminlink."&do=sendMessage" => "Отправить ещё одно сообщение"));
 		break;
 
