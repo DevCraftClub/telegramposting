7b71404f19cbbe565fa485ecf9889f813657be52
MaHarder <info@maxim-harder.de>
2018-06-01 10:39:41 +0200

    Установочный файл

--- install_archive.bat
+++ install_archive.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 mkdir temp
 robocopy upload temp /E
@@ -8,15 +7,4 @@ set PATH=%PATH%;%ProgramFiles%\7-Zip\
 cd ..
 copy /Y temp\telegram_posting.zip install.zip
 rd /s /q temp
-=======
-@echo off
-mkdir temp
-robocopy upload temp /E
-cd temp
-set PATH=%PATH%;%ProgramFiles%\7-Zip\
-7z a -mx0 -r -tzip -aoa telegram_posting.zip *
-cd ..
-copy /Y temp\telegram_posting.zip install.zip
-rd /s /q temp
->>>>>>> 268f80438a471068af16b806ae7d3fdc3f38b55e
 exit;
