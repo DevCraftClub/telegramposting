46743ff023e8e376e4445335a116f9706cd8f039
MaHarder <info@maxim-harder.de>
2022-09-08 20:15:15 +0200

    Исправления ошибок
    - [FIX] Исправлена заявленная ошибка: https://skripters.biz/forum/threads/telegram-posting.50716/#post-497819
    - [FIX] Исправлена заявленная ошибка: Конвертер изображений неверно обрабатывал пути до временных файлов
    - [FIX] Исправлен установщик

--- upload/engine/inc/telegram.php
+++ upload/engine/inc/telegram.php
@@ -14,7 +14,7 @@
 
 $modInfo = [
 	'module_name' => 'Telegram Posting',
-	'module_version' => '1.7.5',
+	'module_version' => '1.7.6',
 	'module_id' => 11,
 	'module_description' => 'Отправка сообщений в телеграм канал или группу',
 	'module_code' => 'telegram',
